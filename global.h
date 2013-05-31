2af68cbfc5f17c744c3e5e09ae1c151d2d2e37d6
Xining Du <xiningdu@gmail.com>
2013-05-31 01:45:19 -0400

    Added global.h in repository.

---
+++ global.h
@@ -0,0 +1,34 @@
+#ifndef __GLOBAL_H
+#define __GLOBAL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Implementation by kernelPBO.cu (compiled with nvcc)
+extern "C" void launch_Mandelbrot_kernel(uchar4*, unsigned int, unsigned int);
+
+// --------------------------------------------------------------------------
+/**
+ * Error handling for cuda functions.
+ * Use macro HANDLE_ERROR( <cuda_function> ).
+ * @param err cudaError_t CUDA Error type
+ * @param file source file (macro calls by __FILE__)
+ * @param line source line (macro calls by __LINE__)
+ * 
+ * Reference: http://developer.nvidia.com/cuda-example-introduction-general-purpose-gpu-programming
+ */
+static void HandleError( cudaError_t err,
+                         const char *file,
+                         int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s (%d) in %s at line %d\n", cudaGetErrorString( err ), err, 
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
+
+// macro for error handling cuda functions
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
+#endif
